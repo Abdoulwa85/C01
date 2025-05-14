f16552eb59612af0d4d4e126f41827a29cdb516d
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-14 19:00:09 +0100

    show_disk_usage.sh modifier

--- show_disk_usage.sh
+++ show_disk_usage.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# Ce script montre l'utilisation du disque
 
-echo "Voici l'utilisation du disque (df -h) :"
+# Exercice 4: Show Disk Usage
+# Ce script affiche l'utilisation du disque de façon lisible
+
+# Affiche l'utilisation globale du disque
+echo "Utilisation du disque:"
 df -h
 
-echo
-echo "Voici la taille de chaque élément ici (du -sh *) :"
-du -sh *
+# Affiche l'utilisation du répertoire courant
+echo "Utilisation du répertoire courant:"
+du -h -d 1 .
