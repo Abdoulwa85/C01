1555581d291a204d08e8632c1b58d9fda61448ee
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-16 20:41:00 +0100

    suppression du fichier count_files.sh

--- count_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Demande à l'utilisateur de saisir le chemin du répertoire
-read a
-# Vérifier que le chemin entré est un repertoire
-n="$(ls "$a" | wc -l)"
-b=$(echo $n)
-echo "Le dossier "$a" contient "$b" fichier(s)."
