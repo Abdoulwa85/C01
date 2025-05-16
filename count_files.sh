24472f1268fc91e8c4a4a672d017c9d4db85028e
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-16 20:38:16 +0100

    modification du fichier count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-‎# ce script compte le nombre de fichiers dans un dossier 
-‎
-‎#Demande à l utilisateur d entre le chemin d un repertoire 
-‎read a
-‎#Verifier le chemin entré est bien un repertoire 
-‎n="$(ls "$a" | ws -l)"
-‎b=$(echo $n)
-‎echo "Le dossier "$a" contient "$b" fichier(s)."
-‎
+#Demande à l'utilisateur de saisir le chemin du répertoire
+read a
+# Vérifier que le chemin entré est un repertoire
+n="$(ls "$a" | wc -l)"
+b=$(echo $n)
+echo "Le dossier "$a" contient "$b" fichier(s)."
