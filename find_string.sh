0583961db08cf4b68c0aee9ce6fc4cf6eb0e078e
Abdoulwa85 <boureimasouleyabdoulwahabou@gmail.com>
2025-05-18 21:00:24 +0100

    modification du fichier find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-# un script qui recherche une chaine spécifique dans un fichier
+
+# Lire le nom du fichier
 read fichier
 
+# Lire la chaine du fichier
 read chaine
 
-if grep -q "$fichier" "$chaine"; then
+# Chercher la chaine dans le fichier avec grep
+if grep "$chaine" "$fichier"; then
+# Si la chaine est trouvée
 echo "La chaine '$chaine' a été trouvée dans $fichier."
 else
+# Si la chaine n'est pas trouvée
 echo "La chaine '$chaine' n'a pas été trouvée dans $fichier."
 fi
